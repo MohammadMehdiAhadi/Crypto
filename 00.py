6aa2288e6f8baba88d51dfe858beea7ebcc0523e
Mohammad <mohammad.ahadi8282@gmail.com>
2024-07-22 17:01:53 +0330

    mamad commited

--- 00.py
+++ 00.py
@@ -11,20 +11,21 @@ print(help(df.ta.indicators()))
 # print(df)
 '''''''''
 aberration, above, above_value, accbands, ad, adosc, adx,
-alma, amat, ao, aobv, apo, aroon, atr, bbands, 
+alma, amat, ao, aobv, apo, bbands, 
 below, below_value, bias, bop, brar, cci, cdl_pattern, cdl_z,
 cfo, cg, chop, cksp, cmf, cmo, coppock, cross, cross_value, 
-cti, decay, decreasing, dema, dm, donchian, dpo, ebsw, efi, ema, 
+cti, decay, decreasing, dema, dm, donchian, dpo, ebsw, efi, 
 entropy, eom, er, eri, fisher, fwma, ha, hilo, hl2, hlc3, hma, hwc,
 hwma, ichimoku, increasing, inertia, jma, kama, kc, kdj, kst, kurtosis,
-kvo, linreg, log_return, long_run, macd, mad, massi, mcgd, median, mfi, 
+kvo, linreg, log_return, long_run,  mad, massi, mcgd, median, mfi, 
 midpoint, midprice, mom, natr, nvi, obv, ohlc4, pdist, percent_return, pgo,
-ppo, psar, psl, pvi, pvo, pvol, pvr, pvt, pwma, qqe, qstick, quantile, rma,
-roc, rsi, rsx, rvgi, rvi, short_run, sinwma, skew, slope, sma, smi, squeeze,
+ppo, psar, psl, pvi, pvo, pvol, pvr, pvt, pwma, qqe, qstick, quantile, 
+ rsx, rvgi, rvi, short_run, sinwma, skew, slope, squeeze,
 squeeze_pro, ssf, stc, stdev, stoch, stochrsi, supertrend, swma, t3, td_seq,
 tema, thermo, tos_stdevall, trima, trix, true_range, tsi, tsignals, ttm_trend, 
-ui, uo, variance, vhf, vidya, vortex, vp, vwap, vwma, wcp, willr, wma, xsignals
+ui, uo, variance, vhf, vidya, vortex, vp, vwma, wcp, willr, wma, xsignals
 zlma, zscore
 
 '''''''''
-
+# roc, rsi, ema,sma, smi,
+# rma,macd, atr, vwap, aroon,
