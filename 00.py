cd63dcca511e8d37f0884002120118b9938346a9
Mohammad <mohammad.ahadi8282@gmail.com>
2024-07-23 15:59:34 +0330

    mamad commited

--- 00.py
+++ 00.py
@@ -1,11 +1,44 @@
 import pandas_ta as ta
 import pandas as pd
 
+# roc, rsi, ema,sma, smi ----> MAMAD
+
 df = pd.DataFrame()
 df = df.ta.ticker("BTC-USD", period="1y", interval="1d")
 
+# df["Tommorw_Close"] = df["Close"].shift(-1)
+# print(df)
+# print(df["Close"])
 
-print(help(df.ta.indicators()))
-# print(help(ta.cci))
-# df["cci"] = ta.cci(df["High"], df["Low"], df["Close"], length=7)
+
+
+
+# print(help(df.ta.indicators()))
+
+#
+# # print(help(ta.roc))
+# df["roc_7"] = ta.roc( df["Close"], length=7)
+#
+# # print(help(ta.rsi))
+# df["rsi_7"] = ta.rsi( df["Close"], length=7)
+#
+# # print(help(ta.ema))
+# df["ema_7"] = ta.ema( df["Close"], length=7)
+#
+# # print(help(ta.sma))
+# df["sma_7"] = ta.sma( df["Close"], length=7)
+
+# print(help(ta.squeeze))
+# sq = ta.squeeze(df["High"], df["Low"], df["Close"])
+# print(sq)
+# df["squeeze"] = sq["SQZ_20_2.0_20_1.5"]
+# print(df)
+
+#
+# print(help(ta.smi))
+# smi= ta.smi(df["Close"])
+# print(smi)
+
+# df["SMI_5_20_5"],df["SMIs_5_20_5"],df["SMIo_5_20_5"]= ta.smi(df["Close"])
+# print(df["SMI_5_20_5"])
 # print(df)
